9d4d4ce9dec160b285340e21f3d4f0aec22f9d9a
taji <taji@sonicgarden.jp>
2024-12-04 11:41:05 +0900

    ルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :daily_reports, only: %i[show] do
     resources :retrospectives, only: %i[show new create edit update destroy], module: :daily_reports
   end
+  resources :chats do
+    resources :messages, only: %i[new create]
+  end
 end
