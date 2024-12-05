af1d66f8df8bced6f8d7baec509074714c24b9b8
taji <taji@sonicgarden.jp>
2024-12-05 15:57:22 +0900

    factのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :daily_reports, only: %i[show] do
     resources :retrospectives, only: %i[show new create edit update destroy], module: :daily_reports
+    resources :facts, only: %i[show new create edit update destroy], module: :daily_reports
   end
 end
