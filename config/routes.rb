1caf0c643d5bbb2e2d377561aeb44631a14f11da
taji <taji@sonicgarden.jp>
2024-12-05 21:05:42 +0900

    resource :retrospective

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :daily_reports, only: %i[show] do
-    resources :retrospectives, only: %i[show new create edit update destroy], module: :daily_reports
-    resources :facts, only: %i[show new create edit update destroy], module: :daily_reports
+    resources :facts, only: %i[show new create edit update destroy], module: :daily_reports do
+      resource :retrospective
+    end
   end
 end
