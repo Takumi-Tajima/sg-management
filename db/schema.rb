2a3b8f725c1e5e1a219d1b7e950ffa4a85e9865c
taji <taji@sonicgarden.jp>
2024-12-06 11:07:07 +0900

    daily_reportsにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_05_135706) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_06_020415) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "daily_reports", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_daily_reports_on_user_id"
   end
 
   create_table "facts", force: :cascade do |t|
@@ -64,6 +66,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_05_135706) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "daily_reports", "users"
   add_foreign_key "facts", "tasks"
   add_foreign_key "messages", "facts"
   add_foreign_key "retrospectives", "facts"
