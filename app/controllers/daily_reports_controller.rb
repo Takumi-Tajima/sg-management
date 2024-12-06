2a65d24e1ac9f9d2718bfe351d559ceb83be5bc5
taji <taji@sonicgarden.jp>
2024-12-06 11:28:39 +0900

    ユーザーと紐づいた日報を作成

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -8,6 +8,7 @@ class DailyReportsController < ApplicationController
   end
 
   def create
-    @daily_report = DailyReport.new
+    @daily_report = current_user.daily_reports.create!
+    redirect_to daily_report_path(@daily_report)
   end
 end
